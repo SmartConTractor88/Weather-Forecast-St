b67c9b75f940f865dfa47c15834e501315ddbc57
SmartConTractor <login.blockchain888@gmail.com>
2025-03-17 13:46:34 +0100

    Add weather data filter in the backend.

--- backend.py
+++ backend.py
@@ -7,10 +7,19 @@ def get_data(place, days=None, option=None):
     url=f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
     response = requests.get(url)
     data = response.json()
-    return data
+    filtered_data = data["list"]
+    nr_values = 8 * days
+    filtered_data = filtered_data[:nr_values]
+
+    if option == "Temperature":
+        filtered_data = [dict["main"]["temp"] for dict in filtered_data]
+    if option == "Sky":
+        filtered_data = [dict["weather"][0]["main"] for dict in filtered_data]
+    
+    return filtered_data
 
 if __name__ == "__main__":
-    print(get_data(place="Leipzig"))
+    print(get_data(place="London", days=2, option="Sky"))
 
 #def get_data(days):
 #    dates = ["1", "2", "3", "4", "5"]
